589895b16f23bbfd8c9f2d97d8e26691304fb5ff
Michael Fiander <mcf42@zips.uakron.edu>
2018-04-16 23:54:52 -0400

    updated json

---
+++ json/json.hpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <vector>
+
+// TODO: Write your JSON class hierarchy here.
---
+++ json/main.cpp
@@ -0,0 +1,22 @@
+
+#include "json.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <iterator>
+
+int
+main(int argc, char* argv[]) 
+{
+  if (argc < 2)
+    return 1;
+  
+  // Read an input file into a string.
+  std::ifstream ifs(argv[1]);
+  std::istreambuf_iterator<char> first(ifs);
+  std::istreambuf_iterator<char> last;
+  std::string str(first, last);
+
+  Value* val = parse(str);
+  val->print();
+}
